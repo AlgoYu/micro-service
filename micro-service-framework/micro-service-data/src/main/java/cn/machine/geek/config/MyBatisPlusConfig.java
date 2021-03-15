c44316b53a43b478d266c2e18c66b8ef088b598f
MachineGeek <794763733@qq.com>
2021-03-15 15:51:23 +0800

    add OptimisticLockerInnerInterceptor

--- micro-service-framework/micro-service-data/src/main/java/cn/machine/geek/config/MyBatisPlusConfig.java
+++ micro-service-framework/micro-service-data/src/main/java/cn/machine/geek/config/MyBatisPlusConfig.java
@@ -2,6 +2,7 @@ package cn.machine.geek.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,5 +26,17 @@ public class MyBatisPlusConfig {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return mybatisPlusInterceptor;
+    }
+
+    /**
+    * @Author: MachineGeek
+    * @Description: 乐观锁插件
+    * @Date: 2021/3/15
+     * @param
+    * @Return: com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor
+    */
+    @Bean
+    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor(){
+        return new OptimisticLockerInnerInterceptor();
     }
 }
